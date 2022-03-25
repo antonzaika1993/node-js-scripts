ad0653f8a8a8ed4e90ce3ec721ffcc66115deb9f
Dmitry Bezruk <espado1512b@gmail.com>
2022-03-25 18:50:15 +0300

    ...

--- scraping_muster_urls.js
+++ scraping_muster_urls.js
@@ -1,3 +1,5 @@
+// noinspection DuplicatedCode
+
 const puppeteer = require("puppeteer-extra");
 const axios = require('axios')
 const mysql = require('mysql');
@@ -28,82 +30,37 @@ let table_urls = "interstitial_master_urls"
 
 function connectToMySql() {
     return new Promise((resolve, reject) => {
-        try {
-            conn = mysql.createConnection({
-                host: DB_HOST,
-                user: DB_USER,
-                password: DB_PASSWORD,
-                database: DB_NAME
+        conn = mysql.createConnection({
+            host: DB_HOST,
+            user: DB_USER,
+            password: DB_PASSWORD,
+            database: DB_NAME
+        });
+        // let n = null.toString() // test raise exception
+        conn.connect(function (err) {
+            if (err) reject(err);
+            conn.query("SELECT * FROM " + table_publishers, function (err, result) {
+                if (err) reject(err);
+                resolve(result)
             });
-            // let n = null.toString() // test raise exception
-            conn.connect(function (err) {
-                if (err) throw err;
-                conn.query("SELECT * FROM " + table_publishers, function (err, result) {
-                    if (err) throw err;
-                    resolve(result)
-                });
-            })
-        } catch (e) {
-            reject(e)
-        }
+        })
     })
 }
 
-function StartScraping() {
-    console.log('*** Start Scraping')
-    let result = []
+function check_axios(pub) {
     return new Promise(async (resolve, reject) => {
-        for (let i in publishers) {
-            let publisher = publishers[i]
-            console.log('*********************************************************************************************')
-            console.log('publisher_link_id = ', publisher.publisher_link_id, ' publisher_url = ', publisher.url)
-            let resp = []
+        try {
             let axios_instance = axios.create();
             axios_instance.defaults.timeout = 5000;
             console.log('axios GET to publisher url')
-            await axios_instance.get(publisher.url, {
+            axios_instance.get(pub.url, {
                 headers: header,
                 timeout: 5000
-            }).then(async (r) => {
+            }).then((r) => {
                 console.log('http status = ', r.status)
-                publishers_http_statuses.push([200, publisher.publisher_link_id])
-                await puppeteer
-                    .launch({
-                        "headless": true,
-                        "args": ["--fast-start", "--disable-extensions", "--no-sandbox"],
-                        "ignoreHTTPSErrors": true
-                    })
-                    .then(async (browser) => {
-                        console.log('we in puppeteer launch then')
-                        const page = await browser.newPage();
-
-                        page.on("response", async (response) => {
-                            for (let keyword in keywords_ad_networks) {
-                                let ad_network = keywords_ad_networks[keyword]
-                                // console.log('keyword = ', keyword)
-                                if (await response._url.includes(keyword)) {
-                                    // console.log('keyword includes')
-                                    resp.push({
-                                        'publisher_link_id': publisher.publisher_link_id,
-                                        'ad_network_term_id': ad_network,
-                                        'master_url': await response._url
-                                    })
-                                }
-                            }
-                        });
-                        await page.setExtraHTTPHeaders(header)
-                        try {
-                            await page.goto(publisher.url, {
-                                waitUntil: "load",
-                                timeout: 15000,
-                            });
-                            await page.browser().close()
-                        } catch (e) {
-                            console.log('Bad url ', publisher.url, ' ', e)
-                            await page.browser().close()
-                        }
-                    });
-            }).catch((e) => {
+                publishers_http_statuses.push([200, pub.publisher_link_id])
+                resolve(true)
+            }).catch(e => {
                 let bad_status = 404
                 if (e.response) {
                     if (e.response.status) {
@@ -111,14 +68,62 @@ function StartScraping() {
                         bad_status = e.response.status
                     }
                 }
-                publishers_http_statuses.push([bad_status, publisher.publisher_link_id])
-                return resp
+                console.log('http status = ', bad_status)
+                publishers_http_statuses.push([bad_status, pub.publisher_link_id])
+                resolve(false)
             })
-            console.log('we out StartScraping. Resp = ', resp)
-
-            return resp
+        } catch {
+            reject(false)
         }
+    })
+}
+
+function StartScraping() {
+    console.log('*** Start Scraping')
+    let scraping_result = []
+    let pup_obj = puppeteer
+    return new Promise(async (resolve, reject) => {
+        try {
+            pup_obj = puppeteer.launch(
+            //     {
+            //     "headless": true,
+            //     // "args": ["--fast-start", "--disable-extensions", "--no-sandbox"],
+            //     "ignoreHTTPSErrors": true
+            // }
+            )
+        } catch (e) {
+            reject(e)
+            return false
+        }
+        await pup_obj.then(async (browser) => {
+            console.log('we in puppeteer launch then')
+            const page = await browser.newPage();
+            await page.setExtraHTTPHeaders(header)
+            for (let i in publishers) {
+                let publisher = publishers[i]
+                console.log('*************************************************************************************')
+                console.log('publisher_link_id = ', publisher.publisher_link_id, ' publisher_url = ', publisher.url)
+                let resp = []
+                if (await check_axios(publisher)) {
+                    try {
+                        await page.goto(publisher.url, {
+                        waitUntil: "load",
+                        timeout: 20000,
+                        });
+                    } catch (e) {
+                        // pass
+                    }
+                } else {
+                    // pass
+                }
+                if (i > 4) {
+                    break
+                }
+            }
+            await page.browser().close()
+        });
         console.log('*** Finished StartScraping')
+        resolve(scraping_result)
     })
 }
 
@@ -146,16 +151,21 @@ const updatePublishers = async _ => {
 async function main() {
     try {
         publishers = await connectToMySql() // list of objects
+        console.log('*** Success getting publishers.\nLength of publishers = ', publishers.length)
     } catch (e) {
-        console.log('Error in get publishers ', e)
+        console.log('!!! Error in get publishers ', e)
         return null
     }
-    console.log('*** Success getting publishers.\nLength of publishers = ', publishers.length)
-    let result = await StartScraping() // Working with every publisher
+    try {
+        let result = await StartScraping() // Working with every publisher
+        console.log('*** Success getting result.\nLength of result = ', result.length)
+    } catch (e) {
+        console.log('!!! Error in get result ', e)
+        return null
+    }
     // insertUrls(conn, table_urls, master_urls_to_insert)
-    await updatePublishers()
+    // await updatePublishers()
     console.log('*** MAIN END ***')
-    return null
 }
 
 let start = main()
